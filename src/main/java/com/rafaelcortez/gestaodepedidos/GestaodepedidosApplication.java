2afcf5cb74540ba4d3173992849179ab1137295d
Rafael Cortez <rafaelcortezp@gmail.com>
2021-06-29 19:01:45 -0300

    Primeiro commit

---
+++ src/main/java/com/rafaelcortez/gestaodepedidos/GestaodepedidosApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcortez.gestaodepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodepedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodepedidosApplication.class, args);
+	}
+
+}
